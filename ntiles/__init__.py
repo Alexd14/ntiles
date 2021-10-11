177f71d9a7df256c5b11873bbf1cb2e86d0a7204
Alexd14 <32649372+Alexd14@users.noreply.github.com>
2021-10-10 23:47:42 -0400

    Update __init__.py

---
+++ ntiles/__init__.py
@@ -0,0 +1,9 @@
+from .ntile_kicker import Ntile
+from .portals.pricing_portal import PricingPortal
+from .portals.sector_portal import SectorPortal
+
+__all__ = [
+    'Ntile',
+    'PricingPortal',
+    'SectorPortal',
+]
