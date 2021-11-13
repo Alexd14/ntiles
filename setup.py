1a2a0781a26d4d3d3d865455a230c28f2b1fe2ec
Alexd14 <32649372+Alexd14@users.noreply.github.com>
2021-11-13 08:55:11 -0500

    PyPi stuff

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='ntiles',
+    version='0.1.0',
+    packages=find_packages(),
+    license='Apache License 2.0',
+    description='Vectorized quantile backtester.',
+    author='Alex DiCarlo',
+    author_email='dicarlo.a@northeastern.edu',
+    url='https://github.com/Alexd14/ntiles-backtester',
+    download_url='https://github.com/Alexd14/ntiles-backtester/archive/refs/tags/v0.1.0.tar.gz',
+    keywords=['factor', 'backtesting', 'alphalens', 'vectorized backtesting', 'equity trading'],
+    install_requires=[
+        'numba',
+        'pandas',
+        'equity-db',
+        'numpy',
+        'matplotlib',
+        'empyrical'
+        ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
