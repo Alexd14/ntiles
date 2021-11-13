5f8dab426ccfa3dce09b5c6e5e9478e6f0131bf7
Alexd14 <32649372+Alexd14@users.noreply.github.com>
2021-11-13 11:14:48 -0500

    PyPi Stuff

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages
 
 setup(
     name='ntiles',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     license='Apache License 2.0',
     description='Vectorized quantile backtester.',
     author='Alex DiCarlo',
     author_email='dicarlo.a@northeastern.edu',
     url='https://github.com/Alexd14/ntiles-backtester',
-    download_url='https://github.com/Alexd14/ntiles-backtester/archive/refs/tags/v0.1.0.tar.gz',
+    download_url='https://github.com/Alexd14/ntiles-backtester/archive/refs/tags/v0.1.1.tar.gz',
     keywords=['factor', 'backtesting', 'alphalens', 'vectorized backtesting', 'equity trading'],
     install_requires=[
         'numba',
